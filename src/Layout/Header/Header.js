ccefe9bcd4aa8d2eb7f8e5d4e4cef2713bbd7f98
Jason Aravanis <j.aravanis@icloud.com>
2021-11-25 16:00:16 +1100

    Adjust header content based on auth state

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../App";
 
 function Header() {
-  return (
-    <div className="flex justify-between items-center">
-      <h1 className="ml-4 text-xl text-secondary">Blog CMS</h1>
-      <nav className="flex justify-end p-4">
-        <NavLink
-          end
-          to="/articles"
-          className={({ isActive }) =>
-            isActive ? "active-nav-link" : "nav-link"
-          }
-        >
-          Home
-        </NavLink>
-        <NavLink to="/signin" className="nav-link">
-          Sign in
-        </NavLink>
-        <NavLink to="/signup" className="highlight-button">
-          Sign up
-        </NavLink>
-      </nav>
-    </div>
-  );
+  const auth = useContext(AuthContext);
+
+  const handleSignOut = (e) => {
+    e.preventDefault();
+    // clearValue()
+    console.log("Activated clearValue function");
+  };
+
+  if (auth) {
+    return (
+      <div className="flex justify-between items-center">
+        <h1 className="ml-4 text-xl text-secondary">Blog CMS</h1>
+        <nav className="flex justify-end p-4">
+          <NavLink
+            end
+            to="/articles"
+            className={({ isActive }) =>
+              isActive ? "active-nav-link" : "nav-link"
+            }
+          >
+            Home
+          </NavLink>
+
+          <button
+            onClick={(e) => handleSignOut(e)}
+            className="nav-link bg-highlight text-white hover:text-white hover:bg-indigo-700"
+          >
+            Sign Out
+          </button>
+        </nav>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex justify-between items-center">
+        <h1 className="ml-4 text-xl text-secondary">Blog CMS</h1>
+        <nav className="flex justify-end p-4">
+          <NavLink to="/signup" className="highlight-button">
+            Sign In
+          </NavLink>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Header;

5c29716ef34a672f9f82f555710ad355523f72b0
Jason Aravanis <j.aravanis@icloud.com>
2021-11-30 18:30:39 +1100

    Refactor Article.js

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -10,7 +10,7 @@ import "tinymce/skins/ui/oxide/skin.min.css";
 import "tinymce/skins/ui/oxide/content.min.css";
 import "tinymce/skins/content/default/content.min.css";
 import { Editor } from "@tinymce/tinymce-react";
-import { useNavigate, useParams } from "react-router";
+import { useParams } from "react-router";
 import useAxios from "../hooks/useAxios";
 import axios from "axios";
 import useLocalStorage from "../hooks/useLocalStorage";
@@ -46,6 +46,20 @@ function Article() {
 
   const [flash, dispatch] = useReducer(flashReducer, "");
 
+  const handleFlashMessage = (result, successMessage) => {
+    if (result.data.message) {
+      dispatch({ type: "FLASH", payload: result.data.message });
+    } else if (result.data.errors) {
+      const errors = result.data.errors.map((obj) => obj.msg);
+      dispatch({ type: "FLASH", payload: errors });
+    } else {
+      dispatch({
+        type: "FLASH",
+        payload: successMessage,
+      });
+    }
+  };
+
   const saveArticle = async () => {
     try {
       dispatch({ type: "RESET" });
@@ -63,20 +77,10 @@ function Article() {
           },
         }
       );
-      if (result.data.message) {
-        dispatch({ type: "FLASH", payload: result.data.message });
-      } else if (result.data.errors) {
-        const errors = result.data.errors.map((obj) => obj.msg);
-        dispatch({ type: "FLASH", payload: errors });
-      } else {
-        dispatch({
-          type: "FLASH",
-          payload: "Article has been saved!",
-        });
-      }
-      console.log(result);
+      handleFlashMessage(result, "Article has been saved!");
     } catch (err) {
       console.log(err);
+      dispatch({ type: "FLASH", payload: err.message });
     }
   };
 
@@ -97,20 +101,10 @@ function Article() {
           },
         }
       );
-      if (result.data.message) {
-        dispatch({ type: "FLASH", payload: result.data.message });
-      } else if (result.data.errors) {
-        const errors = result.data.errors.map((obj) => obj.msg);
-        dispatch({ type: "FLASH", payload: errors });
-      } else {
-        dispatch({
-          type: "FLASH",
-          payload: "Article has been saved and published!",
-        });
-      }
-      console.log(result);
+      handleFlashMessage(result, "Article has been saved and published!");
     } catch (err) {
       console.log(err);
+      dispatch({ type: "FLASH", payload: err.message });
     }
   };
 
@@ -131,20 +125,10 @@ function Article() {
           },
         }
       );
-      console.log(result);
-      if (result.data.message) {
-        dispatch({ type: "FLASH", payload: result.data.message });
-      } else if (result.data.errors) {
-        const errors = result.data.errors.map((obj) => obj.msg);
-        dispatch({ type: "FLASH", payload: errors });
-      } else {
-        dispatch({
-          type: "FLASH",
-          payload: "Article has removed from public view.",
-        });
-      }
+      handleFlashMessage(result, "Article has removed from public view.");
     } catch (err) {
       console.log(err);
+      dispatch({ type: "FLASH", payload: err.message });
     }
   };
 
@@ -173,78 +157,84 @@ function Article() {
       }
     } catch (err) {
       console.log(err);
+      dispatch({ type: "FLASH", payload: err.message });
     }
   };
 
   return (
     <div>
-      <div className="text-highlight my-2">
-        <p className="empty:h-6">{flash}</p>
-      </div>
-      <form className="flex flex-col items-start mb-4">
-        <div className="w-full">
-          <label className="text-2xl text-primary">Title</label>
-          <input
-            type="text"
-            className="border border-gray-300 rounded-lg py-1 px-2 text-secondary w-full mt-2"
-            value={title || ""}
-            onChange={(e) => setTitle(e.target.value)}
+      {isError && <p>Something went wrong</p>}
+      {!isError && (
+        <div>
+          <div className="text-highlight my-2">
+            <p className="empty:h-6">{flash}</p>
+          </div>
+          <form className="flex flex-col items-start mb-4">
+            <div className="w-full">
+              <label className="text-2xl text-primary">Title</label>
+              <input
+                type="text"
+                className="border border-gray-300 rounded-lg py-1 px-2 text-secondary w-full mt-2"
+                value={title || ""}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className="my-4 w-full">
+              <label className="text-2xl text-primary">Description</label>
+              <input
+                type="text"
+                className="border border-gray-300 rounded-lg py-1 px-2 text-secondary w-full mt-2"
+                value={description || ""}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+          </form>
+          <Editor
+            initialValue={article.content}
+            init={{
+              skin: false,
+              content_css: false,
+              font_formats:
+                "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Oswald=oswald; Roboto=roboto; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats",
+              content_style:
+                "@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap'); body { font-family: Roboto; }",
+              height: 600,
+              menubar: false,
+              plugins: ["link image", "table paste"],
+              toolbar:
+                "undo redo | formatselect | fontselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat",
+            }}
+            value={content}
+            onEditorChange={(c) => setContent(c)}
           />
+          <div className="flex justify-start my-4">
+            <button
+              onClick={saveArticle}
+              className="nav-link bg-blue-400 text-white hover:text-white hover:bg-blue-500"
+            >
+              Save
+            </button>
+            <button
+              onClick={publishArticle}
+              className="nav-link bg-emerald-400 text-white hover:text-white hover:bg-emerald-500"
+            >
+              Save and publish
+            </button>
+            <button
+              onClick={unpublishArticle}
+              className="nav-link bg-fuchsia-400 text-white hover:text-white hover:bg-fuchsia-500"
+            >
+              Unpublish
+            </button>
+            <button
+              onClick={deleteArticle}
+              className="nav-link bg-red-400 text-white hover:text-white hover:bg-red-500"
+            >
+              Discard
+            </button>
+          </div>
         </div>
-        <div className="my-4 w-full">
-          <label className="text-2xl text-primary">Description</label>
-          <input
-            type="text"
-            className="border border-gray-300 rounded-lg py-1 px-2 text-secondary w-full mt-2"
-            value={description || ""}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
-      </form>
-      <Editor
-        initialValue={article.content}
-        init={{
-          skin: false,
-          content_css: false,
-          font_formats:
-            "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Oswald=oswald; Roboto=roboto; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats",
-          content_style:
-            "@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap'); body { font-family: Roboto; }",
-          height: 600,
-          menubar: false,
-          plugins: ["link image", "table paste"],
-          toolbar:
-            "undo redo | formatselect | fontselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat",
-        }}
-        value={content}
-        onEditorChange={(c) => setContent(c)}
-      />
-      <div className="flex justify-start my-4">
-        <button
-          onClick={saveArticle}
-          className="nav-link bg-blue-400 text-white hover:text-white hover:bg-blue-500"
-        >
-          Save
-        </button>
-        <button
-          onClick={publishArticle}
-          className="nav-link bg-emerald-400 text-white hover:text-white hover:bg-emerald-500"
-        >
-          Save and publish
-        </button>
-        <button
-          onClick={unpublishArticle}
-          className="nav-link bg-fuchsia-400 text-white hover:text-white hover:bg-fuchsia-500"
-        >
-          Unpublish
-        </button>
-        <button
-          onClick={deleteArticle}
-          className="nav-link bg-red-400 text-white hover:text-white hover:bg-red-500"
-        >
-          Discard
-        </button>
-      </div>
+      )}
     </div>
   );
 }
